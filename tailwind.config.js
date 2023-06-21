9d8591b87463bae9a21d9c403e9bb2314bb62ca2
SARATHKUMAR-T <sarathkumartk98@gmail.com>
2023-06-21 13:00:59 +0530

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+
+   
+      colors: {
+        text1: "#e1f0fe",
+        background: "#000305",
+        primary: "#0606c1",
+        secondary: "#000000",
+        accent: "#6306c1",
+      },
+      fontFamily:{
+        nunito:['Nunito','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+};
